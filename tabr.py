0071e11f6a2abf67612965c1c1c90d0b6d2fbe3b
kirito-night <zengfx4869@gmail.com>
2024-01-18 17:06:44 +0100

    architecture tabr

--- tabr.py
+++ tabr.py
@@ -15,36 +15,73 @@ from tqdm import tqdm
 
 import lib
 
+class Model(nn.Module): 
+    def __init__(
+        self,
+        *,
+        #
+        n_num_features: int,
+        n_bin_features: int,
+        cat_cardinalities: list[int],
+        n_classes: Optional[int],
+        #
+        num_embeddings: Optional[dict],  # lib.deep.ModuleSpec
+        d_main: int, # model dimension
+        d_multiplier: float,
+        encoder_n_blocks: int,
+        predictor_n_blocks: int,
+        mixer_normalization: Union[bool, Literal['auto']],
+        context_dropout: float,
+        dropout0: float,
+        dropout1: Union[float, Literal['dropout0']],
+        normalization: str,
+        activation: str,
+        #
+        # The following options should be used only when truly needed.
+        memory_efficient: bool = False,
+        candidate_encoding_batch_size: Optional[int] = None,
+    ) -> None:
+        super().__init__()
 
-dataset = lib.build_dataset(
-    name="mnist",
-    path="data",
-    download=True,
-    normalize=True,
-    flatten=True,
-    one_hot=False,
-    train_size=0.8,
-    random_state=42,
-    verbose=True,
-)
+        def make_block(norm):
+            if norm:
+                return nn.Sequential(
+                    nn.LayerNorm(d_main), 
+                    nn.Linear(),
+                    nn.ReLU(),
+                    nn.Dropout(),
+                    nn.Linear()
+                    )
+            else: 
+                return nn.Sequential(
+                    nn.Linear(),
+                    nn.ReLU(),
+                    nn.Dropout(),
+                    nn.Linear(), # ?
+                    nn.Dropout(), # ?
+                    )
+        NE, NP = 1, 1
+        self.block_E = [make_block(i>0) for i in range(NE)]
+        self.block_P = [make_block(i>0) for i in range(NP)]
+        self.P = nn.Sequential(
+            nn.LayerNorm(),
+            nn.ReLU(),
+            nn.Linear()
+        )
+        #### R block ####
+        self.S = nn.Linear() # x_dim
+        self.V_y = nn.Linear()
+        self.V_t = nn.Sequential(
+            nn.Linear(bias = False),
+            nn.Dropout(),
+            nn.ReLU(),
+            nn.Linear()
+        )
 
-
-device = torch.device('cuda' if torch.cuda.is_available() else 'mps')
-if dataset.is_regression:
-    dataset.data['Y'] = {k: v.float() for k, v in dataset.Y.items()}
-Y_train = dataset.Y['train'].to(
-    torch.long if dataset.is_multiclass else torch.float
-)
-Y_test = dataset.Y['test'].to(
-    torch.long if dataset.is_multiclass else torch.float
-)
-X_train = dataset.X['train'].to(device)
-X_test = dataset.X['test'].to(device)
-
-for k, v in dataset.X.items():
-    print(k, v.shape)
-for k, v in dataset.Y.items():
-    print(k, v.shape)
     
+
+
+
+
 
 
